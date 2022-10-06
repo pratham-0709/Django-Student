567ec90ad3b535a262d3a6c5b5805a7dba3b4c0d
PRATHAMESH SHUKLA <53365574+pratham-0709@users.noreply.github.com>
2022-10-06 18:52:39 +0530

    Update manage.py
    Some Basic Updates

--- student/manage.py
+++ student/manage.py
@@ -2,11 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.management.commands.runserver import Command as runserver
+#runserver.default_port = "8000"
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student.settings')
+    #runserver.default_port = "8000"
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -19,4 +21,6 @@ def main():
 
 
 if __name__ == '__main__':
+    #from django.core.management.commands.runserver import Command as runserver
+    runserver.default_port = "8000"
     main()
